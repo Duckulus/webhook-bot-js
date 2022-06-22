ff403a3e6dcef1701e331299b5dc78ffd44decfa
Amin <amin.haddou@frg.wwschool.de>
2022-06-22 17:53:51 +0200

    add usercommand example

---
+++ examples/src/usercommand.js
@@ -0,0 +1,24 @@
+import { CommandResponse, WebhookServer } from 'webhook-bot-js';
+
+const server = new WebhookServer({
+  application_id: '',
+  token: '',
+  public_key: '',
+});
+
+// usercommands can be accessed by rightclicking a user in a guild
+server.registerUserCommand({
+  name: 'ping',
+  execute: interaction => {
+    return {
+      type: CommandResponse.Message,
+      data: {
+        content: `<@${interaction.data.target_id}>`,
+      },
+    };
+  },
+});
+
+await server.pushGlobalApplicationCommands();
+
+server.start();
